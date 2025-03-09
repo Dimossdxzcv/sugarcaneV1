c787fe9d9987e549b8960f75d3585f998781258b
Emir <raybebancok@gmail.com>
2025-03-09 18:32:32 +0700

    a

---
+++ src/main/java/net/dimossdxzcv/sugarcanev1/SugarcaneV1client.java
@@ -0,0 +1,10 @@
+package net.dimossdxzcv.sugarcanev1;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SugarcaneV1client implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
